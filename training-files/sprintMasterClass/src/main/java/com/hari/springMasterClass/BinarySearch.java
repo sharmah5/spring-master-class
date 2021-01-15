a0cf36017b16f97b20f0700834a94d8164088aad
sharmah5 <hari.sharma@mail.utoronto.ca>
2021-01-14 22:11:44 -0500

    Used a ConfigurableBeanFactory instead of the explicit 'prototype' string to mark a bean with the prototype scope.

--- training-files/sprintMasterClass/src/main/java/com/hari/springMasterClass/BinarySearch.java
+++ training-files/sprintMasterClass/src/main/java/com/hari/springMasterClass/BinarySearch.java
@@ -2,11 +2,12 @@ package com.hari.springMasterClass;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearch {
 
 	private boolean verbose = false;
