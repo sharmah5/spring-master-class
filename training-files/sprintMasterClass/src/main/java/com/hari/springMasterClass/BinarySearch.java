1688bfad8a04addc48bccbcedcb95a9484f10f1c
sharmah5 <hari.sharma@mail.utoronto.ca>
2021-01-14 22:08:01 -0500

    Creating two variables that reference different instances of a prototype bean.

--- training-files/sprintMasterClass/src/main/java/com/hari/springMasterClass/BinarySearch.java
+++ training-files/sprintMasterClass/src/main/java/com/hari/springMasterClass/BinarySearch.java
@@ -2,9 +2,11 @@ package com.hari.springMasterClass;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class BinarySearch {
 
 	private boolean verbose = false;
